eaee7d3bbdf86e392adac10cc72fb66b58403538
haelldev <haelldev@gmail.com>
2025-09-23 11:53:53 -0300

    Primeiro commit do projeto Liven

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/livenacademy/', // ← nome do seu repositório
-  plugins: [react()]
-});
+  plugins: [react()],
+})
